4c2c606012578c98536a0ffd17d443f1d8db406f
Bonnie Park <newapril.dev@gmail.com>
2021-01-29 16:10:46 +0900

    reimplement the Colour Maker app using UISlider controls instead of switches

--- ColourMaker/ViewController.swift
+++ ColourMaker/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
+    @IBOutlet weak var colourView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func colourChangeConponent(_ sender: Any) {
+        let r : CGFloat = CGFloat(self.redControl.value)
+        let g : CGFloat = CGFloat(self.greenControl.value)
+        let b : CGFloat = CGFloat(self.blueControl.value)
+        
+        colourView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+    
 }
 
